234318bdeb51e66ef59c9cb269b430abc5cbfec9
vtavta30920 <nghechngoc36@gmail.com>
2025-04-09 16:42:14 +0700

    trying deploy

--- FE/vite.config.js
+++ FE/vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: process.env.IOT_BASED_FARM / "/IoT-Based-Farm-Management-System-IBFM",
+});
