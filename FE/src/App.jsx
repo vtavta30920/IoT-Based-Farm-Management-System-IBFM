5ef6147977f9cdaed08152e1ab7dac8fa95ebaaa
vtavta30920 <nghechngoc36@gmail.com>
2025-05-23 19:00:39 +0700

    re-deploy

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -59,8 +59,8 @@ import IotDevices from "./pages/manager/IotDevices.jsx";
 import DeviceDetails from "./pages/manager/DeviceDetails.jsx";
 
 import AdminLayout from "./components/layouts/AdminLayout.jsx";
-import AccountDetail from "./pages/admin/AccountDetail.jsx";
-import CreateAccount from "./pages/admin/CreateAccount.jsx";
+import AccountDetail from "./pages/admin/AccountDetail";
+import CreateAccount from "@/pages/admin/CreateAccount";
 
 const App = () => {
   const queryClient = new QueryClient();
