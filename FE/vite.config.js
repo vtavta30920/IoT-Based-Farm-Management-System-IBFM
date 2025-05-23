6721ddf93b505f3166412667a6e4383756d7a740
vtavta30920 <nghechngoc36@gmail.com>
2025-05-23 19:07:12 +0700

    fail fix

--- FE/vite.config.js
+++ FE/vite.config.js
@@ -1,11 +1,7 @@
-// vite.config.js
 import { defineConfig } from "vite";
-import path from "path";
+import react from "@vitejs/plugin-react";
 
+// https://vite.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  plugins: [react()],
 });
