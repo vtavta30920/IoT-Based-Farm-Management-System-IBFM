32f2d1a24a280d427f537d6f1c6dd7546a61feec
vtavta30920 <nghechngoc36@gmail.com>
2025-05-23 19:14:01 +0700

    trying deploy

--- FE/src/admin/AccountDetail.jsx
+++ FE/src/admin/AccountDetail.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
-import { useUpdateRole, useUpdateAccount } from "../../api/AccountEndPoint";
-import defaultAvatar from "../../assets/avatardefault.jpg";
+
+import defaultAvatar from "../assets/avatardefault.jpg";
+import { useUpdateAccount, useUpdateRole } from "../api/AccountEndPoint";
 
 const AccountDetail = () => {
   const location = useLocation();
--- FE/src/admin/CreateAccount.jsx
+++ FE/src/admin/CreateAccount.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useCreateAccount } from "../api/AccountEndPoint";
-import defaultAvatar from "../../assets/avatardefault.jpg";
+import defaultAvatar from "../assets/avatardefault.jpg";
 
 const CreateAccount = () => {
   const navigate = useNavigate();
