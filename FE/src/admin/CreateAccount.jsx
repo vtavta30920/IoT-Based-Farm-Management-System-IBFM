de61434239602e951319908d945cd694eb5cd7c4
vtavta30920 <nghechngoc36@gmail.com>
2025-05-23 19:11:33 +0700

    change file location

--- FE/src/admin/CreateAccount.jsx
+++ FE/src/admin/CreateAccount.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { useCreateAccount } from "../../api/AccountEndPoint";
+import { useCreateAccount } from "../api/AccountEndPoint";
 import defaultAvatar from "../../assets/avatardefault.jpg";
 
 const CreateAccount = () => {
