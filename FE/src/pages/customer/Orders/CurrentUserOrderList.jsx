b4bcfacfc9f3fccb198ea2f5a25fe31ed1fe995d
vtavta30920 <nghechngoc36@gmail.com>
2025-05-30 13:57:56 +0700

    not optional

--- FE/src/pages/customer/Orders/CurrentUserOrderList.jsx
+++ FE/src/pages/customer/Orders/CurrentUserOrderList.jsx
@@ -600,7 +600,7 @@ const CurrentUserOrderList = () => {
             </div>
             <div className="mb-4">
               <label className="block text-gray-700 text-sm font-bold mb-2">
-                Comment (optional):
+                Comment:
               </label>
               <textarea
                 className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-500"
